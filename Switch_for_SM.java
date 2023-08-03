4430c4dae7888244551915e719840d43501cc2f3
Fatum678 <140236211+Fatum678@users.noreply.github.com>
2023-08-03 23:12:31 +0800

    Update Switch_for_SM.java

--- Switch_for_SM.java
+++ Switch_for_SM.java
@@ -1,22 +1,41 @@
+package UserLoginSystem;
 
-package oodjassignment;
 import java.util.Scanner;
 
-public class Switch_for_SM {
+abstract public class Switch_for_SM {
+
+    public static void CallSwitchCase() {
         System.out.println("Sale Manager\nMenu \n\n1. Item Entry \n2. Supplier Entry \n3. Daily sales \n4. Create Purchase Requisition \n5. View Purchase Requisition \n6. View Purchase Order \n0. Log out");
         System.out.println("Enter number to continue: \n");
         Scanner userInput = new Scanner(System.in);
         int UserInput = userInput.nextInt();
-        String bullshit;
-        switch(UserInput){
-            case 1: bullshit = "Item entry will be developed"; break;
-            case 2: bullshit = "Supplier entry will be developed";break;
-            case 3: bullshit = "Daily sale will be developed";break;
-            case 4: bullshit = "Create PR will be developed";break;
-            case 5: bullshit = "View PR will be developed";break;
-            case 6: bullshit = "View PO Will be developed";break;
-            case 0: bullshit = "Log out session will be developed";break;
-            default: bullshit = "Invalid number input"; break;
+        String bullshit = null;
+        switch (UserInput) {
+            case 1:
+                ItemDetails.AddItem();
+                break;
+            case 2:
+                SupplierEntry.runSupplierEntry();
+                break;
+            case 3:
+                DailySales.dailyItemWiseSalesEntry();
+                break;
+            case 4:
+                bullshit = "Create PR will be developed";
+                break;
+            case 5:
+                bullshit = "View PR will be developed";
+                break;
+            case 6:
+                bullshit = "View PO Will be developed";
+                break;
+            case 0:
+                bullshit = "Log out session will be developed";
+                break;
+            default:
+                bullshit = "Invalid number input";
+                break;
         }
         System.out.println(bullshit);
+    }
 }
