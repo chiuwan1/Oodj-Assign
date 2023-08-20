7c3f0e1b431616c1a5b80391cdbc6b5f6c61e208
tegantee <140231468+tegantee@users.noreply.github.com>
2023-08-20 14:57:30 +0800

    Add files via upload

---
+++ DisplaySupplierList.java
@@ -0,0 +1,29 @@
+
+package assignment;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Scanner;
+
+public class DisplaySupplierList {
+    
+    static void displaySupplierList(){
+        List<String> DisplaySL = new ArrayList<>();
+        try (BufferedReader Listreader = new BufferedReader(new FileReader("SupplierListraw.txt"))){
+            
+            System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+            String line;
+            while ((line = Listreader.readLine())!=null)
+            {
+                System.out.println(line);
+            }
+
+}catch(IOException e) {System.out.println("Error");}
+    
+    }
+    
+}
