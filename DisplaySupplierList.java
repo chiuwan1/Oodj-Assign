c6f8ef31d895b38ceb63d21e7413f9217678a5db
tegantee <140231468+tegantee@users.noreply.github.com>
2023-08-31 17:39:40 +0800

    DisplaySupplierList.java
    Latest

--- DisplaySupplierList.java
+++ DisplaySupplierList.java
@@ -5,23 +5,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.Scanner;
-
 public class DisplaySupplierList {
-    
-    static void displaySupplierList(){
+
+    static void displaySupplierList() {
         List<String> DisplaySL = new ArrayList<>();
-        try (BufferedReader Listreader = new BufferedReader(new FileReader("SupplierListraw.txt"))){
-            
+        try (BufferedReader Listreader = new BufferedReader(new FileReader("SupplierList.txt"))) {
+
+            System.out.println("-----------------------------------------------------------------------------------------------------------------------");
+            System.out.printf("%-15s | %-15s | %-25s | %-25s | %s\n", "Supplier Code", "Supplier Name", "Supplier Phone Number", "Supplier Email Address", "Item ID");
             System.out.println("-----------------------------------------------------------------------------------------------------------------------");
             String line;
-            while ((line = Listreader.readLine())!=null)
-            {
-                System.out.println(line);
+            while ((line = Listreader.readLine()) != null) {
+                String[] displaySupplierList = line.split(";");
+                String SupplierCode = displaySupplierList[0];
+                String SupplierName = displaySupplierList[1];
+                String SupplierPN = displaySupplierList[2];
+                String SupplierEA = displaySupplierList[3];
+                String ItemID = displaySupplierList[4];
+                //System.out.println(line); 
+                System.out.printf("%-15s | %-15s | %-25s | %-25s | %s\n", SupplierCode, SupplierName, SupplierPN, SupplierEA, ItemID);
             }
 
-}catch(IOException e) {System.out.println("Error");}
-    
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
     }
-    
 }
